a0496f6cea70f54dcc44492c7fbeb5bf9c067ef2
Hendra Wijaya <wijaya.hndra@gmail.com>
2022-07-17 21:38:49 +0700

    Movie API

---
+++ src/themes/themes.js
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        type: 'dark',
+        primary: {
+          main: '#0A1929',
+        },
+        secondary: {
+          main: '#1976d2',
+        },
+      },
+});
+
+export default theme;
