9901865ca58567448c6c81cef50c68b5849a6613
dedy270895 <dedospratamos@gmail.com>
2022-07-18 22:05:58 +0700

    finished

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,6 @@
 import { BottomNavigation, BottomNavigationAction, Box } from "@mui/material";
 import React from "react";
-import RestoreIcon from '@mui/icons-material/Restore';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
-import { styled } from '@mui/material/styles';
+
 
 
 const Footer = () => {
