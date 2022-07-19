c05ce41190f87dc431d9361870ec7ddf9769fde1
edri4s <edri.adisetiawan@gmail.com>
2022-07-19 12:39:24 +0800

    buat halaman home sementara

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import { Paper } from "@mui/material";
+import { Container } from "@mui/system";
+import React, { components } from "react";
+
+export default class Home extends components {
+    render() {
+        return (
+            <Container>
+                <Paper>
+                    <h1>Ini Home</h1>
+                </Paper>
+            </Container>
+        )
+    }
+}
