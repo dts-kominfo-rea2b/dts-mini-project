0ade3317282b99784eb85f586e570a8fc36a3554
Yoverina <yoverina_nurdin@yahoo.com>
2022-07-19 18:52:43 +0700

    add theme

--- src/App.js
+++ src/App.js
@@ -4,19 +4,23 @@ import Login from './pages/login'
 import Register from './pages/register'
 import MovieDetail from './pages/movie-detail'
 import NotFound from './pages/not-found'
+import { ThemeProvider } from "@mui/material";
 
+import theme from "./styles/theme";
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="login" element={<Login/>}/>
-        <Route path="register" element={<Register/>}/>
-        <Route path="movie-detail/:id" element={<MovieDetail/>}/>
-        <Route path="*" element={<NotFound/>}/>
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="movie-detail/:id" element={<MovieDetail />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
---
+++ src/styles/theme.js
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+      type: 'dark',
+      primary: {
+        main: '#141414',
+      },
+      secondary: {
+        main: '#B9090B',
+      },
+    },
+  });
+
+export default theme;
