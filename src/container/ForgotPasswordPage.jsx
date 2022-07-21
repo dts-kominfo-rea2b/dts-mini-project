20d867f8731e073e7addca730e9b1869fc8f4d17
Budianto IP <budiantoip@gmail.com>
2022-07-21 19:45:30 +0700

    modify auth pages

--- src/container/ForgotPasswordPage.jsx
+++ src/container/ForgotPasswordPage.jsx
@@ -1,9 +1,82 @@
-import React from 'react'
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import { sendPasswordResetEmail } from 'firebase/auth';
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+
+import { auth } from '../config/firebase';
 
 const ForgotPasswordPage = () => {
+  const [successMessage, setSuccessMessage] = React.useState("");
+  const [errorMessage, setErrorMessage] = React.useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const email = data.get("email");
+
+    try {
+      await sendPasswordResetEmail(auth, email);
+      setErrorMessage("");
+      setSuccessMessage("a password reset email has been sent to " + email);
+    } catch (error) {
+      setSuccessMessage("");
+      setErrorMessage(error.message);
+    }
+  };
+
   return (
-    <div>ForgotPasswordPage</div>
-  )
+    <Container component='main' maxWidth='xs'>
+      <Box
+        sx={{
+          mt: 10,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component='h1' variant='h5'>
+          Sign in
+        </Typography>
+        <Box component='form' onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin='normal'
+            required
+            fullWidth
+            id='email'
+            label='Email Address'
+            name='email'
+            autoComplete='email'
+            autoFocus
+          />
+          <Typography color='blue'>{successMessage}</Typography>
+          <Typography color='red'>{errorMessage}</Typography>
+          <Button
+            type='submit'
+            fullWidth
+            variant='contained'
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Send Password Reset Link
+          </Button>
+          <Grid container>
+            <Grid item>
+              <Link to='/login'>{"Sign in"}</Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  );
 }
 
 export default ForgotPasswordPage
