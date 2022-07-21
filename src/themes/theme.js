8cdae0f08f4115743088ec7b77722a98a851b827
tiarroelpridatamba1108 <tiarrotamba24@gmail.com>
2022-07-21 20:22:22 +0700

    add footer in home page

--- src/themes/theme.js
+++ src/themes/theme.js
@@ -1,15 +1,24 @@
-import { createTheme } from '@mui/material/styles';
+import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
-    palette: {
-        type: 'dark',
-        primary: {
-          main: '#141414',
-        },
-        secondary: {
-          main: '#EFCA08',
-        },
-      },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 900,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
+  palette: {
+    type: "dark",
+    primary: {
+      main: "#141414",
+    },
+    secondary: {
+      main: "#EFCA08",
+    },
+  },
 });
 
 export default theme;
